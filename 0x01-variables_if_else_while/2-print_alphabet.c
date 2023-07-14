289f988ad00caca8d7d7ba7ca38e495e39dacb42
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-14 15:36:20 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Finally) 
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+ {
+	 putchar(c);
+ }
+	putchar('\n');
+
+	return (0);
+}
+
