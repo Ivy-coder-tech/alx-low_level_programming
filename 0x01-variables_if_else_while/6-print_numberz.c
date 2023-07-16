479caf73f955f7ebaba0ee6d674536334d2154de
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-16 08:23:49 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of coding
+ * Return: 0 (Finally)
+ */
+
+int main(void)
+{
+	int k;
+
+	k = 0;
+	while
+		(k < 10)
+		{
+			putchar(k + '0');
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
