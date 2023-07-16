22c956acbc6198842c85f7e5e24ec6268b041f38
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-16 07:50:20 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Start of prog
+ * Return: 0 (Perfect)
+ */
+
+int main(void)
+{
+	char reverse;
+
+	reverse = 'z';
+	while
+		(reverse >= 'a')
+		{
+			putchar(reverse);
+			reverse--;
+
+		}
+	putchar ('\n');
+	return (0);
+}
