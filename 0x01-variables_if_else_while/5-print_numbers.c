cca286adbcab86dc11718749bceb93b24346387e
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-16 07:21:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Code Entry
+ * Return: 0 (Always)
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		printf("%d", j);
+	putchar('\n');
+	return (0);
+}
