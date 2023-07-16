f85ecd0cb35e76c28936f96fd394bb18bdc255b1
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-16 08:42:36 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Allows us to write our code
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char h;
+
+	int a;
+
+	h = 'a';
+	a = '0';
+while
+(a < 10)
+{
+	putchar(a + '0');
+	a++;
+}
+while
+(h <= 'f')
+{
+	putchar(h);
+	h++;
+}
+putchar('\n');
+return (0);
+}
+
