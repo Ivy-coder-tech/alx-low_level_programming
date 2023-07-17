80367fb8a03f5fb02fabe2f3eec051ff883d4b23
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-17 01:46:31 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - An alx program that prints a line with puts function
- * Retun: 0
+ * Return: 0 (Always)
  */
-int main (void)
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
 
