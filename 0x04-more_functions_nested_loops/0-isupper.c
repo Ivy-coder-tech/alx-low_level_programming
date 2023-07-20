86fd39dac48fa751e38465e79fc1e4843965df05
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-07-20 08:03:36 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,14 @@
 
 /**
  * _isupper - Check if a letter is supper
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
