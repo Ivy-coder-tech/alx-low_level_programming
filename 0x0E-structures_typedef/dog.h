9e28150ba879e97d9d1c610e4fc03f8f990885bd
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-08-14 09:20:02 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: Name of my dog
+ * @age: agr of my dog
+ * @owner: Owner of the dog
+ */
+
+struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif
