b2e1f7d8117aeb8ffa75d1d697e6c12194fe358a
Ivy-coder-tech <ivymutuku22@gmail.com>
2023-08-24 07:54:42 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,10 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %d\n", h->len, h->len);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
